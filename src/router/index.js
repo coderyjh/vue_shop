c1a5e3690b188a2c8fd186c5bd31e8c27e18a76a
杨佳华 <8256773+yangjiahua77@user.noreply.gitee.com>
2021-01-29 11:16:46 +0800

    12.登录退出

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,38 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
-
+import Home from '../components/Home.vue'
+// import { from } from 'core-js/fn/array'
 Vue.use(VueRouter)
 
-const routes = [
-    { path: '/', redirect: '/Login' },
-    { path: '/Login', component: Login }
+const routes = [{
+        path: '/',
+        redirect: '/login'
+    },
+    {
+        path: '/login',
+        component: Login
+    },
+    {
+        path: '/Home',
+        component: Home
+    }
 ]
 
 const router = new VueRouter({
     routes
+})
+
+// 挂载路由导航守卫，to 表示将要访问的路径，from 表示从哪里来，next 是下一个要做的操作
+router.beforeEach((to, from, next) => {
+    // 直接放行
+    if (to.path === '/login') return next()
+        // 获取 token
+    const tokenStr = window.sessionStorage.getItem('token')
+        // token 不存在，跳转登录界面
+    if (!tokenStr) return next('/login')
+        // token 存在，放行
+    next()
 })
 
 export default router
