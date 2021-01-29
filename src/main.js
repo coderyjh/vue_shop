55f26a3d7089dae80e7ef3b88f801f6517eedd73
杨佳华 <8256773+yangjiahua77@user.noreply.gitee.com>
2021-01-29 15:41:30 +0800

    gay2

--- src/main.js
+++ src/main.js
@@ -11,8 +11,22 @@ import './assets/css/global.css'
 
 import axios from 'axios'
 axios.defaults.baseURL = 'http://192.168.88.122:8888/api/private/v1/'
-Vue.prototype.$http = axios
 
+//请求在到达服务器之前，先会调用use中的这个回调函数来添加请求头信息
+axios.interceptors.request.use((config) => {
+        //为请求头对象，添加token验证的Authorization字段
+        config.headers.Authorization = window.sessionStorage.getItem('token')
+        return config
+    })
+    // 无效 token 的处理
+axios.interceptors.response.use((res) => {
+    if (res.data.meta.msg === '无效token' && res.data.meta.status === 400) {
+        location.href = '/#/login'
+    }
+    return res
+})
+
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 new Vue({
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,9 +1,26 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Header } from 'element-ui'
+import {
+    Container,
+    Aside,
+    Main,
+    Menu,
+    Submenu,
+    // MenuItemGroup,
+    MenuItem
+} from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Header)
+Vue.use(Container)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+    // Vue.use(MenuItemGroup)
+Vue.use(MenuItem)
     // 进行全局挂载：
 Vue.prototype.$message = Message
